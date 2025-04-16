d7a29220299c4946b77d680a4eb2b9824d90befe
Rayan Syed <rsyed@scc1.bu.edu>
2025-04-16 00:15:46 -0400

    naive implementation

---
+++ src/dft/dft.h
@@ -0,0 +1,17 @@
+#ifndef DFT_H
+#define DFT_H
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <webgpu/webgpu.hpp>
+#include "../webgpu_utils.h"
+
+void dft(
+    WebGPUContext& context, 
+    wgpu::Buffer& outputBuffer, 
+    std::vector<std::vector<std::complex<float>>>& input
+);
+
+#endif 
